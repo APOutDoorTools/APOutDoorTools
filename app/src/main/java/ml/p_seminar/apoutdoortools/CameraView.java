13f568855559a520f8a829a83e8799b15f96f76c
APOutDoorTools <info@p-seminar.ml>
2017-09-26 20:36:06 +0200

    CameraView hinzugefügt

---
+++ app/src/main/java/ml/p_seminar/apoutdoortools/CameraView.java
@@ -0,0 +1,67 @@
+package ml.p_seminar.apoutdoortools;
+
+import android.content.Context;
+import android.hardware.Camera;
+import android.util.AttributeSet;
+import android.view.SurfaceView;
+import android.view.SurfaceHolder;
+
+import java.io.IOException;
+
+import static android.view.SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS;
+
+/**
+ * Created by lthyr_000 on 26.09.2017.
+ */
+
+public class CameraView extends SurfaceView implements SurfaceHolder.Callback {
+
+    SurfaceHolder surfaceHolder;
+    Camera camera;
+
+    public CameraView(Context context, AttributeSet attrs) {
+        super(context,attrs);
+        surfaceHolder=getHolder();
+        surfaceHolder.addCallback(this);
+        //noinspection deprecation
+        surfaceHolder.setType(SURFACE_TYPE_PUSH_BUFFERS);
+    }
+    public CameraView(Context context){
+        super(context);
+        surfaceHolder=getHolder();
+        surfaceHolder.addCallback(this);
+        //noinspection deprecation
+        surfaceHolder.setType(SURFACE_TYPE_PUSH_BUFFERS);
+    }
+
+
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+        camera = Camera.open();
+        try {
+            camera.setPreviewDisplay(holder);
+        } catch (IOException e) {
+            e.printStackTrace();
+            camera.release();
+            camera=null;
+        }
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+        try {
+            Camera.Parameters parameters = camera.getParameters();
+            parameters.setPreviewSize(width, height);
+            camera.setParameters(parameters);
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        camera.startPreview();
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+
+    }
+}
