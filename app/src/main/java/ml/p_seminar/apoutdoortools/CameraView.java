fe2810d996b12ca1a35fce824ce12873ff3f6c70
felix121felix <felix121felix@gmail.com>
2017-09-26 22:01:13 +0200

    nix

--- app/src/main/java/ml/p_seminar/apoutdoortools/CameraView.java
+++ app/src/main/java/ml/p_seminar/apoutdoortools/CameraView.java
@@ -16,6 +16,8 @@ import static android.view.SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS;
 
 public class CameraView extends SurfaceView implements SurfaceHolder.Callback {
 
+
+    //nix
     SurfaceHolder surfaceHolder;
     Camera camera;
 
