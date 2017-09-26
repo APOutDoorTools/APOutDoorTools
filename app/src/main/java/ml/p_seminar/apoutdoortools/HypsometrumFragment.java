de77a7abaefe2459fc52411e6b2fdef1e792ae5c
APOutDoorTools <info@p-seminar.ml>
2017-09-26 19:52:55 +0200

    Fragment Manager hinzugefügt, um zwischen den Views umzuschalten.

---
+++ app/src/main/java/ml/p_seminar/apoutdoortools/HypsometrumFragment.java
@@ -0,0 +1,24 @@
+package ml.p_seminar.apoutdoortools;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by lthyr_000 on 26.09.2017.
+ */
+
+public class HypsometrumFragment extends Fragment {
+
+    private View view;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        view=inflater.inflate(R.layout.hypsometrum,container,false);
+        return view;
+    }
+}
--- app/src/main/java/ml/p_seminar/apoutdoortools/MainActivity.java
+++ app/src/main/java/ml/p_seminar/apoutdoortools/MainActivity.java
@@ -1,8 +1,10 @@
 package ml.p_seminar.apoutdoortools;
 
+import android.app.Fragment;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.FragmentManager;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -23,6 +25,7 @@ public class MainActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        /*
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -31,6 +34,7 @@ public class MainActivity extends AppCompatActivity
                         .setAction("Action", null).show();
             }
         });
+        */
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -79,8 +83,13 @@ public class MainActivity extends AppCompatActivity
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
+        android.app.FragmentManager bernd=getFragmentManager();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_hypsometrum) {
+            bernd.beginTransaction().replace(R.id.content_frame,new HypsometrumFragment()).commit();
+        } else if (id == R.id.nav_nubibusmeter) {
+            bernd.beginTransaction().replace(R.id.content_frame,new nubibusmeterFragment()).commit();
+        } else if (id == R.id.nav_camera) {
             // Handle the camera action
         } else if (id == R.id.nav_gallery) {
 
