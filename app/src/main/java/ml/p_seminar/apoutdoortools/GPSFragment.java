2c3d64d2c287670898e645eb88913fd402fff14b
Tobias <T_obias@gmail.com>
2017-10-06 10:49:29 +0200

    GPS added

---
+++ app/src/main/java/ml/p_seminar/apoutdoortools/GPSFragment.java
@@ -0,0 +1,133 @@
+package ml.p_seminar.apoutdoortools;
+
+
+import android.Manifest;
+import android.app.Fragment;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+
+
+
+
+
+public class GPSFragment extends Fragment{
+    private Button button;
+    private TextView textView;
+
+    private LocationManager locationManager;
+    private LocationListener locationListener;
+    private int zustand;
+
+
+    @Override
+   public void onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate();
+
+        super.onCreate(savedInstanceState);
+        getActivity().setContentView(R.layout.activity_main);
+
+        button = (Button) getActivity().findViewById(R.id.button);
+
+        textView=(TextView) getView().findViewById(R.id.textView);
+        textView.setText("Koordinaten:");
+        textView.setTextSize(20);
+
+        zustand=0;
+
+
+        locationManager = (LocationManager) getgetSystemService(LOCATION_SERVICE);
+
+        locationListener = new LocationListener()
+        {
+            @Override
+            public void onLocationChanged(Location location)
+            {
+
+                textView.setText("Koordinaten\n\n "+location.getLatitude()+ "\n" + location.getLongitude() + "\nProvider:\t" + location.getProvider() + "\nHöhe:\t" + location.getAltitude() + "\nGenauigkeit:\t" + location.getAccuracy()+" mögliche Abweichung in Metern\n");
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider)
+            {
+                Intent hilfe = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                startActivity(hilfe);
+            }
+        };
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)     //Hier wird geprüft, ob eine bestimmte SDK vorhanden ist.
+        {
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                requestPermissions(new String[]{
+                        Manifest.permission.ACCESS_FINE_LOCATION,Manifest.permission.ACCESS_COARSE_LOCATION,Manifest.permission.INTERNET
+                }, 0);
+            }
+            return;
+        }
+        else
+        {
+            knopfInitialisieren();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults)
+    {
+        switch(requestCode)
+        {
+            case 0:
+                if(grantResults.length>0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                    knopfInitialisieren();
+                return;
+        }
+    }
+
+    private void knopfInitialisieren()
+    {
+        button.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+
+                if(zustand==0)
+                {
+                    button.setText("Stop");
+                    textView.setText("GPS Signal wird gesucht");
+                    locationManager.requestLocationUpdates("gps", 500, 0, locationListener); //(wodurch das Signal zur verfügung gestellt wird, Zeit in Millisekunden, nach der der Standrt erneut überprüft werden soll,Distanz in Metern, nach der der Standort erneut überprüft werden soll)
+                    zustand=1;
+                }
+                else
+                {
+                    button.setText("Position Anfragen");
+                    textView.setText("Koordinaten");
+                    locationManager.removeUpdates(locationListener);
+                    zustand=0;
+                }
+            }
+        });
+
+    }
+}
